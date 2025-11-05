4af537de31edba5f17e09e24274ebc0c53b6af7d
Vladyslav_Hrytsak <vlad.123jj@gmail.com>
2025-11-05 16:20:37 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+/*------TASK 1----*/
+
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+
+let num1  = 1;
+let num2  = 10;
+let num3  = -999;
+let num4  = 123;
+let num5  = 3.14;
+let num6  = 2.7;
+let num7  = 16;
+
+let bool1 = true;
+let bool2 = false;
+
+console.log(hello, owu, com, ua, num1, num2, num3, num4, num5, num6, num7, bool1, bool2);
+
+
+/*------TASK 2----*/
+
+
+let firstName = `Vladyslav`
+let middleName = `Hrytsak`
+let lastName = `Vasylovich`
+
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person);
+
+/*------TASK 3----*/
+
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a, a);
+console.log(typeof b, b);
+console.log(typeof c, c);
+
+/*------TASK 4----*/
+
+
+const name = prompt("Введіть ваше ім'я:");
+const fatherName = prompt("Введіть ваше по-батькові:");
+const age = prompt("Введіть ваш вік:");
+
+console.log("Імʼя:", name);
+console.log("По-батькові:", fatherName);
+console.log("Вік:", age);
+
+
+
+
